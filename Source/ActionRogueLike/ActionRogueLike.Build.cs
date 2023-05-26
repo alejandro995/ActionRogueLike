292c6ce6702d115024b55bbbeed9bded7530a939
Alejandro Daza Truque <alejodt995@gmail.com>
2023-05-26 00:25:09 -0500

    AI, Test Queries and Ai Task
    To make more dinamic the behavior of a Behaviour Tree use Test Queries and custom AI Task to give your AIPawn more control.

--- Source/ActionRogueLike/ActionRogueLike.Build.cs
+++ Source/ActionRogueLike/ActionRogueLike.Build.cs
@@ -7,10 +7,10 @@ public class ActionRogueLike : ModuleRules
 	public ActionRogueLike(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine","InputCore","AIModule", "GameplayTasks" });
+		
+		PublicDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
---
+++ Source/ActionRogueLike/Private/AI/SBTTask_RangeAttack.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/SBTTask_RangeAttack.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/Character.h"
+
+EBTNodeResult::Type USBTTask_RangeAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+
+	AAIController* AIController = OwnerComp.GetAIOwner();
+
+	if(ensure(AIController))
+	{
+		ACharacter* AIPawn = Cast<ACharacter>(AIController->GetPawn());
+
+		if(AIPawn == nullptr)
+		{
+			return EBTNodeResult::Failed;
+		}
+
+		FVector MuzzleLocation = AIPawn->GetMesh()->GetSocketLocation("Muzzle_01");
+
+		AActor* TargetActor = Cast<AActor>(OwnerComp.GetBlackboardComponent()->GetValueAsObject("TargetActor"));
+		if(TargetActor == nullptr)
+		{
+			return EBTNodeResult::Failed;
+		}
+
+		FVector Direction = TargetActor->GetActorLocation() - MuzzleLocation;
+		FRotator MuzzleRotation = Direction.Rotation();
+
+		FActorSpawnParameters Params;
+		Params.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+		AActor* NewProjectile = GetWorld()->SpawnActor<AActor>(NewProjectileClass, MuzzleLocation, MuzzleRotation, Params);
+
+		return NewProjectile ? EBTNodeResult::Succeeded : EBTNodeResult::Failed;
+	}
+
+	return EBTNodeResult::Failed;
+	
+}
